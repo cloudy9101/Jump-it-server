76769363f234be6125c235c26047aef88e4801ca
cloudy9101 <cloudy9101@gmail.com>
2019-07-17 12:08:55 +1200

    ADD: Async handler

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { signUp, signIn } = require('../controllers/users');
+const asyncHandler = require('../utils/asyncHandler');
 
 router.get('/test', (req, res) => {
   // throw new PermissionDeny("User denied");
 });
 
-router.post('/signup', signUp);
+router.post('/signup', asyncHandler(signUp));
 
-router.post('/signin', signIn);
+router.post('/signin', asyncHandler(signIn));
 
 module.exports = router;
